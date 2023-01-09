3bde4102714ed7b990328d064bb7ab86fab5b551
hoangskyht <105069259+hoangskyht@users.noreply.github.com>
2023-01-09 22:55:37 +0700

    Update getrules.sh

--- getrules.sh
+++ getrules.sh
@@ -1,4 +1,4 @@
 curl -X GET \
-"https://api.cloudflare.com/client/v4/zones/e7a7241a366f3106ece154bc1a9062/firewall/rules" \
+"https://api.cloudflare.com/client/v4/zones/62e7a7241a366f3106ece154bc1a9062/firewall/rules" \
 -H "X-Auth-Email: skyht.pro2@gmail.com" \
 -H "X-Auth-Key: 4504a8b04b2154146390921d763eb4edf47dd"
